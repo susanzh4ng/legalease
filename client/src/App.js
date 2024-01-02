b3cc5a910c336fc7a57257b6c92ee823ca0baa9d
susanzh4ng <Susan.Zhang901@gmail.com>
2024-01-02 11:45:59 -0600

    webscraping done, set up routes, began integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from "axios";
 
 function App() {
+  const [def, setDef] = useState('');
+  useEffect(() => {
+    const getUrl = 'http://localhost:5000/api/definition';
+    axios
+      .get(getUrl)
+      .then(function (response) {
+        setDef(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("Error fetching definition due to "+error+"! Please try again!");
+      })
+  }, []);
+
   return (
+
     <div className="App">
-      hiiiii
+      <h3>legalease</h3>
+      <p>{def}</p>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,1 +1,47 @@
-console.log("hey susan");
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const cors = require('cors')
+const router = express.Router()
+const dotenv = require('dotenv').config() //loading .env's content
+const port = process.env.PORT || 5000
+const url = 'https://www.uscourts.gov/glossary/plea' //the website we are scraping from
+
+const app = express() //calling and storing express
+app.use(cors()) //enables CORS for routes
+app.use(express.json()) //parse JSON
+
+
+
+
+/*
+@desc   Get definition
+@route  GET /api/definition
+@access Private
+*/
+app.get('/api/definition', (req, res) => {
+    axios(url)
+    .then(response => {
+        const html = response.data //the html content from url
+        const page = cheerio.load(html) //loading html into a cheerio object
+        let definition = ''
+
+        page('.taxonomy-term-description', html).each(function() { //picking out the definition text
+            definition = page(this).find('p').text() //getting the text from the <p> tag of the "taxonomy-term-description" div
+        })
+        definition = definition.replace(/[\n\t"]/g, '').trim(); //removing JS special charcters
+        res.json({message: definition})
+    }).catch(err => console.log(err))
+})
+
+/*
+@desc   Set definition
+@route  POST /api/definition
+@access Private
+*/
+app.post('/api/definition', (req, res) => {
+    res.status(200).json({message: 'set definition'})
+})
+
+//module.exports = router
+app.listen(port, () => console.log(`Server started on port ${port}`)) //listening out to port 5000 to see if any changes are made
