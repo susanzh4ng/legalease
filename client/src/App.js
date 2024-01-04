c40ce4e4c842eee34e794c0d5ad5f14ebd24da0e
susanzh4ng <Susan.Zhang901@gmail.com>
2024-01-04 16:20:05 -0600

    error handling, added logos

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import {useState, useEffect} from 'react';
 import axios from "axios";
 
 function App() {
-  const [def, setDef] = useState('');
+  const [def, setDef] = useState(''); //storing definition from server
   useEffect(() => {
-    const getUrl = 'http://localhost:5000/api/definition';
+    const getUrl = 'http://localhost:5000/api/definition'; //backend route
     axios
       .get(getUrl)
       .then(function (response) {
-        setDef(response.data.message);
+        setDef(response.data.message); //updates def state with server's data
       })
       .catch(function (error) {
         console.log(error);
@@ -21,7 +21,7 @@ function App() {
 
     <div className="App">
       <h3>legalease</h3>
-      <p>{def}</p>
+      <p>{def}</p> {/*displaying definition*/}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,24 +2,39 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
 const cors = require('cors')
+const asyncHandler = require('express-async-handler')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const router = express.Router()
 const dotenv = require('dotenv').config() //loading .env's content
 const port = process.env.PORT || 5000
-const url = 'https://www.uscourts.gov/glossary/plea' //the website we are scraping from
-
+const url = 'https://www.uscourts.gov/glossary/sentence' //the website we are scraping from
 const app = express() //calling and storing express
 app.use(cors()) //enables CORS for routes
 app.use(express.json()) //parse JSON
+app.use(express.urlencoded({ extended: false }))
 
 
-
+/*
+@desc   Set definition
+@route  POST /api/definition
+@access Private
+*/
+app.post('/api/definition', asyncHandler(async (req, res, next) => {
+    if(!req.body.text) {
+        const error = new Error('Please add a text field!')
+        res.status(400)
+        next(error) //pass to errorHandler
+        return;
+    }
+    res.status(200).json({message: 'set yassss'})
+}))
 
 /*
 @desc   Get definition
 @route  GET /api/definition
 @access Private
 */
-app.get('/api/definition', (req, res) => {
+app.get('/api/definition', asyncHandler(async (req, res) => {
     axios(url)
     .then(response => {
         const html = response.data //the html content from url
@@ -32,16 +47,8 @@ app.get('/api/definition', (req, res) => {
         definition = definition.replace(/[\n\t"]/g, '').trim(); //removing JS special charcters
         res.json({message: definition})
     }).catch(err => console.log(err))
-})
+}))
 
-/*
-@desc   Set definition
-@route  POST /api/definition
-@access Private
-*/
-app.post('/api/definition', (req, res) => {
-    res.status(200).json({message: 'set definition'})
-})
 
-//module.exports = router
+app.use(errorHandler) //can use custom error handler
 app.listen(port, () => console.log(`Server started on port ${port}`)) //listening out to port 5000 to see if any changes are made
